090c324243f12348f914c7d57523a8253d55bb2e
Vasu Goyal <Vasu_Goyal@epam.com>
2025-04-11 12:55:02 +0530

    Writing file with prettier

--- headapps/company/src/lib/apollo-client.js
+++ headapps/company/src/lib/apollo-client.js
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 // Replace this with your Sitecore XM Cloud GraphQL endpoint
 const SITECORE_GRAPHQL_API = process.env.GRAPH_QL_ENDPOINT;
@@ -7,8 +7,8 @@ const createApolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({ uri: SITECORE_GRAPHQL_API }),
     fetchOptions: {
-        method: 'GET',  // Ensure it's using POST
-      },
+      method: 'GET', // Ensure it's using POST
+    },
     cache: new InMemoryCache(),
   });
 };
